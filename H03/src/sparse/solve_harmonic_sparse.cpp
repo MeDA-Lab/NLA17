6780019cad2c6b672cb686af7d8967af25165f13
William Liao <r05246012@ntu.edu.tw>
2017-09-25 15:34:30 +0800

    add cud lu solver for H03

---
+++ H03/src/sparse/solve_harmonic_sparse.cpp
@@ -0,0 +1,33 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// @file    solve_harmonic_sparse.cpp
+/// @brief   The implementation of harmonic problem solving. (sparse version)
+///
+/// @author  Nul
+///
+
+#include <harmonic.hpp>
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// @todo  To be implemented!
+///
+void solveHarmonicSparse(
+  const int     nv,
+  const int     nb,
+  const double *Lii_val,
+  const int    *Lii_row,
+  const int    *Lii_col,
+  const double *Lib_val,
+  const int    *Lib_row,
+  const int    *Lib_col,
+  double       *U
+) {
+  static_cast<void>(nv);
+  static_cast<void>(nb);
+  static_cast<void>(Lii_val);
+  static_cast<void>(Lii_row);
+  static_cast<void>(Lii_col);
+  static_cast<void>(Lib_val);
+  static_cast<void>(Lib_row);
+  static_cast<void>(Lib_col);
+  static_cast<void>(U);
+}
