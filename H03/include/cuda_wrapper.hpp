58ab4778ce11528d9bcdaeab69f67f0834c28c92
William Liao <r05246012@ntu.edu.tw>
2017-09-26 11:15:58 +0800

    add qr wrappers to the header file

--- H03/include/cuda_wrapper.hpp
+++ H03/include/cuda_wrapper.hpp
@@ -92,4 +92,60 @@ void chol_dev(
     const double *b,
     double *x
 );
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// @brief  QR linear system solver wrapper on host.
+///
+/// @param[in]  m       size of the matrix.
+///
+/// @param[in]  nnz     number of nonzero elements in the matrix.
+///
+/// @param[in/out]  A_row     CSR row pointer; pointer.
+///
+/// @param[in/out]  A_col     CSR column index; pointer.
+///
+/// @param[in/out]  A_val  nonzero values of the matrix; pointer.
+///
+/// @param[in]  b        RHS of the linear system; pointer.
+///
+/// @param[out] x        estimated eigenvector w.r.t. mu; pointer.
+///
+/// @note  All inputs should be stored on host.
+///
+void qr_host(
+    int m,
+    int nnz,
+    const double *A_val,
+    const int *A_row,
+    const int *A_col,
+    const double *b,
+    double *x
+);
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// @brief  QR linear system solver wrapper on device.
+///
+/// @param[in]  m       size of the matrix.
+///
+/// @param[in]  nnz     number of nonzero elements in the matrix.
+///
+/// @param[in/out]  A_row     CSR row pointer; pointer.
+///
+/// @param[in/out]  A_col     CSR column index; pointer.
+///
+/// @param[in/out]  A_val  nonzero values of the matrix; pointer.
+///
+/// @param[in]  b        RHS of the linear system; pointer.
+///
+/// @param[out] x        estimated eigenvector w.r.t. mu; pointer.
+///
+/// @note  All inputs should be stored on host.
+///
+void qr_dev(
+    int m,
+    int nnz,
+    const double *A_val,
+    const int *A_row,
+    const int *A_col,
+    const double *b,
+    double *x
+);
 #endif  // SCSC_SGP_HPP
