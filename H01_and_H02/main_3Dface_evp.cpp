487d780b4ee63605fd58ad82780ce00c6ab62b11
William Liao <r05246012@ntu.edu.tw>
2017-09-27 20:33:15 +0800

    change default mu0

--- H01_and_H02/main_3Dface_evp.cpp
+++ H01_and_H02/main_3Dface_evp.cpp
@@ -62,7 +62,7 @@ int main( int argc, char** argv ) {
 
   // Solve EVP
   cout << "Solving Eigenvalue Problem ......................." << flush;
-  double mu0 = 1.0, mu;
+  double mu0 = 1.1, mu;
   double *x;
   x = new double[nv-nb];
   char flag = 'D';
