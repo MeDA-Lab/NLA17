b5e5abae1b32aeb8aff93ec9880fa1cf76a01ed1
William Liao <r05246012@ntu.edu.tw>
2017-09-28 14:57:04 +0800

    set shift sigma to 1e-5

--- lab02/sgp_main_ls.cpp
+++ lab02/sgp_main_ls.cpp
@@ -68,8 +68,8 @@ int main( int argc, char** argv ){
     // Construct Laplacian
     int *csrRowIndA, *csrColIndA;
     double  *csrValA;
-    double shift_sigma = 0.5; // Modify shift_sigma to set the
-                              // shift
+    double shift_sigma = 1e-5; // Modify shift_sigma to set the
+                               // shift
     cout << "Construct Laplacian matrix of graph........." << flush;
     GraphLaplacian(&nnz, cooRowIndA, cooColIndA, cooValA, n, &csrRowIndA, &csrColIndA, &csrValA, shift_sigma);
     cout << " Done.  " << endl;
