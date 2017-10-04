ea5ab7620c9aae06f5faaaf431d819566355c31b
William Liao <r05246012@ntu.edu.tw>
2017-10-04 16:41:13 +0800

    test solving linear system of graph laplacian

--- lab02/src/core/graph_adjacency.cpp
+++ lab02/src/core/graph_adjacency.cpp
@@ -16,7 +16,7 @@
 #include <vector>
 #include <cuda_runtime.h>
 #include "cusparse.h"
-#include <mkl.h>
+#include "mkl.h"
 
 using namespace std;
 
@@ -27,7 +27,7 @@ int GraphAdjacency(int *E, int E_size,
 	int *d_cooRowIndA, *d_cooColIndA;
 	double  *d_val, *d_val_sorted;
 	double *tmp_array, beta = 1.0;
-	vector<double> v1 (E_size , 1.0);
+	vector<double> v1 (2*E_size , 1.0);
 	cusparseHandle_t handle;
 	cusparseIndexBase_t idxBase = CUSPARSE_INDEX_BASE_ZERO;
 	cusparseStatus_t stat;
@@ -47,53 +47,16 @@ int GraphAdjacency(int *E, int E_size,
 
 	if( flag == 'S' )
 	{
-		int  *job, *csrRowInd, *csrColInd, *cooRowInd, *cooColInd;
-		double  *csrVal, *cooVal;
-
-		cooRowInd = new int[E_size];
-		cooColInd = new int[E_size];
-		cooVal    = new double[E_size];
-
-		job = new int[6];
-		*nnz = E_size;
-		copy(E , E+E_size , cooRowInd);
-		copy(E+E_size, E+2*E_size, cooColInd);
-		copy(v1.begin(), v1.end(), cooVal);
+		*cooRowIndA = new int[2*E_size];
+		*cooColIndA = new int[2*E_size];
+		*cooValA    = new double[2*E_size];
 
 		// A+trans(A)
-		csrVal    = new double[*nnz];
-		csrRowInd = new int[*n+1];
-		csrColInd = new int[*nnz];
-
-		job[0] = 2;
-	  	job[1] = 1;
-	  	job[2] = 0;
-	  	job[4] = (*n)*(*n);
-	  	job[5] = 0;
-		mkl_dcsrcoo(job, n, csrVal, csrColInd, csrRowInd, nnz, cooVal, cooRowInd, cooColInd, &info);
-		delete cooVal;
-		delete cooRowInd;
-		delete cooColInd;
-		cooRowInd = new int[*n+1];
-		int nzmax = (*n)*(*n);
-		mkl_dcsradd(&trans, &request, &sort, n, n, csrVal, csrColInd, csrRowInd, &beta, csrVal, csrColInd, csrRowInd, cooVal, cooColInd, cooRowInd, &nzmax, &info);
-		assert( info == 0 );
-		*nnz = cooRowInd[*n]-1;
-		request = 2;
-		cooVal    = new double[*nnz];
-		cooColInd = new int[*nnz];
-		mkl_dcsradd(&trans, &request, &sort, n, n, csrVal, csrColInd, csrRowInd, &beta, csrVal, csrColInd, csrRowInd, cooVal, cooColInd, cooRowInd, &nzmax, &info);
-		assert( info == 0 );
-
-		job[0] = 0;
-		job[4] = *nnz;
-		job[5] = 3;
-		*cooValA    = new double[*nnz];
-		*cooRowIndA = new int[*nnz];
-		*cooColIndA = new int[*nnz];
-		mkl_dcsrcoo(job, n, cooVal, cooColInd, cooRowInd, nnz, *cooValA, *cooRowIndA, *cooColIndA, &info);
-		//cout << "info = " << info << endl;
-		assert( info == 0 );
+		*nnz = 2*E_size;
+		copy(E , E+2*E_size , *cooRowIndA);
+		copy(E+E_size, E+2*E_size, *cooColIndA);
+		copy(E, E+E_size, (*cooColIndA)+E_size);
+		copy(v1.begin(), v1.end(), *cooValA);
 	}else if( flag == 'W' ){
 		//cout << "debug W" << endl;
 		*nnz = E_size;
