e0a0f58a825ebea53e96723b2a0e0c7d3cea379c
William Liao <r05246012@ntu.edu.tw>
2017-10-12 15:10:40 +0800

    fix errors

--- H05/src/magma/solve_sgp_sparse_magma.cpp
+++ H05/src/magma/solve_sgp_sparse_magma.cpp
@@ -11,6 +11,30 @@
 #include "magma_lapack.h"
 using namespace std;
 
+magma_int_t
+magma_dcsrset_gpu(
+    magma_int_t m,
+    magma_int_t n,
+    magmaIndex_ptr row,
+    magmaIndex_ptr col,
+    magmaDouble_ptr val,
+    magma_d_matrix *A,
+    magma_queue_t queue )
+{
+    A->num_rows = m;
+    A->num_cols = n;
+    magma_index_t nnz;
+    magma_index_getvector( 1, row+m, 1, &nnz, 1, queue );
+    A->nnz = (magma_int_t) nnz;
+    A->storage_type = Magma_CSR;
+    A->memory_location = Magma_DEV;
+    A->dval = val;
+    A->dcol = col;
+    A->drow = row;
+
+    return MAGMA_SUCCESS;
+}
+
 void solveGraph(
     int m,
     int nnz,
@@ -53,7 +77,7 @@ void solveGraph(
   magma_d_precondsetup(dA, drhs,
     &dopts.solver_par, &dopts.precond_par, queue);
   // Solve
-  magma_d_solver(dLii, drhs, &dx, &dopts, queue);
+  magma_d_solver(dA, drhs, &dx, &dopts, queue);
   // Get Info
   magma_dsolverinfo(&dopts.solver_par, &dopts.precond_par, queue);
   magma_getvector(m, sizeof(double), dx.dval, 1, x, 1, queue);
